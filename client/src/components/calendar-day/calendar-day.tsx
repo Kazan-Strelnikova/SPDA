ec68763d868e58fe1cdebfb276794f68fdab0ad7
justcgh9 <a.kabardiyadi@innopolis.university>
2025-04-08 14:10:14 +0300

    feat: calendar day component

---
+++ client/src/components/calendar-day/calendar-day.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react"
+import { EventNote, EventNoteProps } from "../event-note/event-note";
+import styles from "./calendar-day.module.scss"
+import { Chip, Divider, Typography } from "@mui/material";
+
+interface CalendarDayProps {
+    day : number;
+    events : EventNoteProps[];
+}
+
+export const CalendarDay : FC<CalendarDayProps> = ({day, events}) => {
+    return (<div className={styles.calendarDay}>
+    
+    <Chip className={styles.dayIndex} label={day}/>
+    
+    <div className={styles.eventsContainer}>
+    {events.length <= 0 
+    ? <Typography variant="body2">События отсутствуют</Typography>
+    : events.map((event, idx) => 
+        <>
+            <Divider className={styles.divider}/>
+            <EventNote isSignedUp={event.isSignedUp} name={event.name} time={event.time} category={event.category}></EventNote>
+            {idx === events.length - 1 && <Divider className={styles.divider}/>}
+        </>
+    )
+    }
+    </div>
+    </div>);
+}
