c18d80b7c08f7a65f7ae2eac2bef82abfce3f5f7
justcgh9 <a.kabardiyadi@innopolis.university>
2025-04-08 14:17:38 +0300

    update calendar day component

--- client/src/components/calendar-day/calendar-day.tsx
+++ client/src/components/calendar-day/calendar-day.tsx
@@ -9,21 +9,25 @@ interface CalendarDayProps {
 }
 
 export const CalendarDay : FC<CalendarDayProps> = ({day, events}) => {
-    return (<div className={styles.calendarDay}>
-    
-    <Chip className={styles.dayIndex} label={day}/>
-    
-    <div className={styles.eventsContainer}>
-    {events.length <= 0 
-    ? <Typography variant="body2">События отсутствуют</Typography>
-    : events.map((event, idx) => 
-        <>
-            <Divider className={styles.divider}/>
-            <EventNote isSignedUp={event.isSignedUp} name={event.name} time={event.time} category={event.category}></EventNote>
-            {idx === events.length - 1 && <Divider className={styles.divider}/>}
-        </>
-    )
-    }
-    </div>
+    return (
+    <div className={styles.calendarDay}>
+        
+        <Chip className={styles.dayIndex} label={day}/>
+        
+        <div className={styles.eventsContainer}>
+            {events.length <= 0 
+            ? 
+            <>
+                <Divider className={styles.divider}/>
+                <Typography variant="body2">События отсутствуют</Typography>
+            </>
+            : events.map((event, idx) => 
+                <>
+                    <Divider className={styles.divider}/>
+                    <EventNote isSignedUp={event.isSignedUp} name={event.name} time={event.time} category={event.category}></EventNote>
+                    {idx === events.length - 1 && <Divider className={styles.divider}/>}
+                </>
+            )}
+        </div>
     </div>);
 }
