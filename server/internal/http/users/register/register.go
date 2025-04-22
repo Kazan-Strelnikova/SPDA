24fc3a1b3b158102bfdd051077059337fc0f0194
justcgh9 <a.kabardiyadi@innopolis.university>
2025-04-22 22:01:23 +0300

    cookie fix 2?

--- server/internal/http/users/register/register.go
+++ server/internal/http/users/register/register.go
@@ -71,8 +71,8 @@ func New(log *slog.Logger, service UserService, timeout time.Duration) func(c *g
 			Value:    token,
 			MaxAge:   3600 * 24 * 365,
 			Path:     "/",
-			Secure:   true,
-			HttpOnly: true,
+			Secure:   false,
+			HttpOnly: false,
 			SameSite: http.SameSiteNoneMode,
 		})
 
